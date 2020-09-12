682c61ab04606bc63acf6d48d6d46097dbe67b47
HwanHeeChung <69942732+HwanHeeChung@users.noreply.github.com>
2020-09-11 21:45:37 -0500

    Avance estructuras de decisión
    Agregué la función eval() para poder recibir funciones por parte del usuario. También, agregué una estructura de decisión para averiguar si los límites inferiores y superiores son coherentes.

--- integral calculator.py
+++ integral calculator.py
@@ -2,7 +2,6 @@
 Euler=2.7182818284
 
 #Definimos las funciones seno, coseno y tangente utilizando la serie de Taylor hasta el 5to término.
-
 def sin(var):
     value=var-(var**3)/6+(var**5)/120-(var**7)/5040
     return value
@@ -16,24 +15,26 @@ def tan(var):
     return value
 
 #Recibe del usuario la función. Por el momento usaremos la función sin(x)/x.
+function=str(input("Input the integrand in terms of x. \nMake sure it does not contain 1/x of any form. \n"))
 
 #Recibe del usuario los datos.
 lower_limit=1
 upper_limit=2
-
-#Parte la distancia entre límites en 1000 partes
+#Parte la distancia entre límites en 10000 partes
 interval=(upper_limit-lower_limit)/10000
 
-#Suma de Riemann
-#Definir variables del bucle de antemano.
-step = lower_limit
-Area=0
+#El límite inferior debe ser menor al límite superior
+if lower_limit > upper_limit:
+    print('Switch lower and upper limits.')
+elif lower_limit == upper_limit:
+    print('The area is 0.')
+else: #Suma de Riemann
+    #Definir variables del bucle de antemano.
+    step = lower_limit
+    Area=0
 
-while step <= upper_limit:
-    Area=(sin(step)/step)*interval+Area
-    step = step+interval
-    print(Area)
-    print(step)
-    print(sin(step)/step)
-if step > upper_limit:
-    print("The value is", Area)
+    while step <= upper_limit:
+        Area=(eval(function,{"x":step,"sin":sin, "cos":cos, "tan":tan, "e":Euler}))*interval+Area
+        step = step+interval
+    if step > upper_limit:
+        print("The value is", Area)
